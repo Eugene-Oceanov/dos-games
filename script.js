c2fcaf254ba9db6c67206d58e469e1987b357961
Eugene Oceanov <kenoby1994@gmail.com>
2025-02-15 19:27:57 +0300

    some styles

--- script.js
+++ script.js
@@ -5,14 +5,26 @@ dos = document.querySelector("#dos");
 
 sidemenuHook.onclick = () => sidemenu.classList.toggle("sidemenu--hidden");
 
+fetch("./library.json")
+    .then(response => response.json)
+    .then(data => console.log(data))
+
 sidemenuItems.forEach(item => {
     item.onclick = (e) => {
-        let title = item.getAttribute("data-title");
         let src = item.getAttribute("data-src");
-        console.log(title);
+        sidemenu.classList.add("sidemenu--hidden");
         dos.style.display = "block";
         Dos(dos, {
             url: src,
         });
     }
 })
+
+function getMenuItem(data) {
+    const item = document.createElement("DIV");
+    item.classList.add("sidemenu-item");
+    item.getAttribute("title", data.title);
+    item.getAttribute("src", data.src);
+    item.innerHTML = `<img class="sidemenu-item__icon" src="${data.iconSrc ? data.iconSrc : "./assets/icons/msdos.png"}" alt="${data.iconAlt}">
+                        <h3 class="sidemenu-item__title">${data.title}</h3>`
+}
